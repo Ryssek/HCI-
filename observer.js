3d301439d4c222a37303ddb0c4a0bd693043ca84
Ryssek <russelmen123@gmail.com>
2024-04-08 03:21:24 -0700

    yeah

--- observer.js
+++ observer.js
@@ -16,7 +16,61 @@ const observer = new IntersectionObserver(
 	}
 );
 
-const tags = document.querySelectorAll("section");
+const observer1 = new IntersectionObserver(
+	(entries) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add("in-view-1");
+				entry.target.classList.remove("not-in-view");
+			} else {
+				entry.target.classList.remove("in-view-1");
+				entry.target.classList.add("not-in-view");
+			}
+		});
+	},
+	{
+		rootMargin: "0px",
+		threshold: [0, 0.1, 1],
+	}
+);
+
+const observer2 = new IntersectionObserver(
+	(entries) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add("in-view-2");
+				entry.target.classList.remove("not-in-view");
+			} else {
+				entry.target.classList.remove("in-view-2");
+				entry.target.classList.add("not-in-view");
+			}
+		});
+	},
+	{
+		rootMargin: "0px",
+		threshold: [0, 0.1, 1],
+	}
+);
+
+const observer3 = new IntersectionObserver(
+	(entries) => {
+		entries.forEach((entry) => {
+			if (entry.isIntersecting) {
+				entry.target.classList.add("in-view-3");
+				entry.target.classList.remove("not-in-view");
+			} else {
+				entry.target.classList.remove("in-view-3");
+				entry.target.classList.add("not-in-view");
+			}
+		});
+	},
+	{
+		rootMargin: "0px",
+		threshold: [0, 0.1, 1],
+	}
+);
+
+const tags = document.querySelectorAll(".animate-on-view");
 
 tags.forEach((tag) => {
 	observer.observe(tag);
